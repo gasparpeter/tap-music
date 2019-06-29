24db2c799e10560c851bba1544a57684e0b3f6f9
Peter Gaspar <petihappygaspar@gmail.com>
2019-06-29 14:52:27 +0300

    - added eventListener to pads, sounds is working

--- tapmusic.js
+++ tapmusic.js
@@ -1,1 +1,14 @@
-console.log("asd")
+window.addEventListener("load", () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+
+
+    pads.forEach((pad, index) => {
+        pad.addEventListener("click", function () {
+            sounds[index].play();
+        });
+    });
+
+});
+
+
